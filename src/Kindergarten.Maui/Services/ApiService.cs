f1ec68608993ad0d689f4c61ec7e38d013f87565
Jaber <gitservice@outlook.com>
2026-05-19 21:20:31 +0000

    fix: use HttpClientHandler with SSL bypass for Android

--- src/Kindergarten.Maui/Services/ApiService.cs
+++ src/Kindergarten.Maui/Services/ApiService.cs
@@ -15,15 +15,11 @@ public class ApiService
 
     public ApiService()
     {
-#if ANDROID
-        var handler = new Xamarin.Android.Net.AndroidMessageHandler
+        var handler = new HttpClientHandler
         {
             ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true
         };
         _client = new HttpClient(handler)
-#else
-        _client = new HttpClient()
-#endif
         {
             BaseAddress = new Uri(Constants.ApiBaseUrl),
             Timeout     = TimeSpan.FromSeconds(30)
