decf2da0be59f97fde01a8670aca70061bacb111
Jaber <gitservice@outlook.com>
2026-05-25 08:09:23 +0000

    fix: Allow SuperAdmin access to PermissionGroupsController

--- src/Kindergarten.Api/Controllers/PermissionGroupsController.cs
+++ src/Kindergarten.Api/Controllers/PermissionGroupsController.cs
@@ -12,7 +12,7 @@ namespace Kindergarten.Api.Controllers;
 
 [ApiController]
 [Route("api/permission-groups")]
-[Authorize(Roles = "Admin")]
+[Authorize(Roles = "Admin,SuperAdmin")]
 public class PermissionGroupsController : ControllerBase
 {
     private readonly ApplicationDbContext _db;
